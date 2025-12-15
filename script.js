79831a0762d18de0c5756012dc273c62dc838483
Eric Gee <egee@wrightbev,com>
2025-12-15 15:18:51 -0500

    main html finalized,

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-console.log('Hello World');
+console.log('Hey what are you doing here?');
+
+// Progress bar
+
+var progress = 0;
+
+function updateProgressBar(correctAnswer) {
+
+}
+
+// Form logic
+
+// Cookie clicker game
+
 var score = 0;
