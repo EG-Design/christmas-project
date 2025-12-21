5465e0d27ee1274ceaf59705399bde2757627de8
Eric Gee <captaingee34@gmail.com>
2025-12-20 23:01:17 -0500

    JS functions added, final update.

--- script.js
+++ script.js
@@ -1,12 +1,64 @@
-console.log("Hey what are you doing here?");
+console.log("Hello world");
 
 // Progress bar
 
-var progress = 0;
+// let currentStep = 0;
+// const totalSteps = 3;
 
-function updateProgressBar(correctAnswer) {}
+// const progressBar = document.getElementById("progress-bar");
+
+// changeStep(1);
+
+// function changeStep(amount) {
+//   currentStep += amount;
+
+//   if (currentStep < 0) currentStep = 0;
+//   if (currentStep > totalSteps) currentStep = totalSteps;
+
+//   progressBar.value = currentStep;
+// }
 
 // Form logic
+
+const form1 = document.getElementById("form1");
+const form2 = document.getElementById("form2");
+const form3 = document.getElementById("form3");
+
+form1.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const input = document.getElementById("question-1");
+  const userAnswer = input.value.trim();
+  const correctAnswer = "0";
+  if (userAnswer === correctAnswer) {
+    input.setAttribute("aria-invalid", "false");
+  } else {
+    input.setAttribute("aria-invalid", "true");
+  }
+});
+
+form2.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const input = document.getElementById("question-2");
+  const userAnswer = input.value.trim();
+  const correctAnswer = "0";
+  if (userAnswer === correctAnswer) {
+    input.setAttribute("aria-invalid", "false");
+  } else {
+    input.setAttribute("aria-invalid", "true");
+  }
+});
+
+form3.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const input = document.getElementById("question-3");
+  const userAnswer = input.value.trim();
+  const correctAnswer = "0";
+  if (userAnswer === correctAnswer) {
+    input.setAttribute("aria-invalid", "false");
+  } else {
+    input.setAttribute("aria-invalid", "true");
+  }
+});
 
 // Cookie clicker game
 
